87c757ee3b3511d5c6759e1a594ea2ea1f250718
Max Argus <argus.max@gmail.com>
2024-11-20 14:35:46 +0100

    plot

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -N ClusterHelloWorld
+#PBS -N octagon
 #PBS -S /bin/bash
-#PBS -l nodes=1:ppn=1:gpus=1,mem=4000mb,nice=10,walltime=00:10:00
+#PBS -l nodes=1:ppn=4:gpus=1,mem=8000mb,nice=10,walltime=22:00:00
 #PBS -m a
 #PBS -M velikanm@informatik.uni-freiburg.de
 #PBS -j oe
@@ -13,6 +13,6 @@ conda activate pfp_env
 python -c "import torch; print(torch.cuda.is_available())"
 export PYTHONPATH=$PYTHONPATH:/home/argusm/lang/RAFT/core
 cd PointFlowMatch
-python scripts/train.py log_wandb=True dataloader.num_workers=0 task_name=unplug_charger +experiment=pointflowmatch_so3
+python scripts/train.py log_wandb=True dataloader.num_workers=2 task_name=unplug_charger +experiment=pointflowmatch_so3
 echo "Hello World"
 exit 0
